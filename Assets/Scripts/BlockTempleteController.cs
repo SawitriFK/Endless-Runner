fbbf81671c97dce5acfeeb7df45996abe80cf6b0
SawitriFK <sawitrifinak@gmail.com>
2021-09-15 18:25:34 +0700

    Second Upload

---
+++ Assets/Scripts/BlockTempleteController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTempleteController : MonoBehaviour
+{
+    private float movement = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        pos.x = transform.position.x + movement * Time.deltaTime;
+        transform.position = pos;
+    }
+}
